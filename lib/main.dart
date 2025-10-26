9f5fe16d9114642489a6c370d1e887ac247a6091
yakupkahraman <falconyakup@gmail.com>
2025-10-26 10:23:18 +0300

    api key deleted and hided

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:heylex/core/router_manager.dart';
 import 'package:heylex/core/theme/theme_provider.dart';
 import 'package:heylex/features/auth/providers/user_answers_provider.dart';
@@ -8,10 +9,10 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Supabase.initialize(
-    url: 'https://ucexexydpoelxkcfdxdt.supabase.co',
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVjZXhleHlkcG9lbHhrY2ZkeGR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEzMjg3NzQsImV4cCI6MjA3NjkwNDc3NH0.LHoGLJs8fSsJkmt7VMpWyAWTuNDlGK8A-JUE2g9EnC8',
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
   );
 
   runApp(
