22428946b54acfe18a487facaacc97d666b94ac3
yakupkahraman <falconyakup@gmail.com>
2025-10-26 09:57:47 +0300

    plans page updated

--- lib/features/home/pages/my_plans.dart
+++ lib/features/home/pages/my_plans.dart
@@ -23,48 +23,157 @@ class MyPlans extends StatelessWidget {
           },
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: GlassEffectContainer(
-          child: Padding(
-            padding: const EdgeInsets.all(24.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  'Free Plan',
-                  style: TextStyle(
-                    fontFamily: "OpenDyslexic",
-                    fontSize: 18,
-                    color: ThemeConstants.orangeColor,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              GlassEffectContainer(
+                child: Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        'Premium Plan',
+                        style: TextStyle(
+                          fontFamily: "OpenDyslexic",
+                          fontSize: 18,
+                          color: ThemeConstants.orangeColor,
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      Text(
+                        'Mevcut planınız premium plandır. ',
+                        style: TextStyle(
+                          fontFamily: "OpenDyslexic",
+                          fontSize: 14,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        '\n -Ai Analizleri ve Kişiselleştirilmiş Öneriler\n \n -Tüm Mini Oyunlara Erişim \n\n -Her Oyun için Ayda 100 Tur Hakkı ',
+                        style: TextStyle(
+                          fontFamily: "OpenDyslexic",
+                          fontSize: 12,
+                        ),
+                        textAlign: TextAlign.start,
+                      ),
+                      /*
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                            ThemeConstants.orangeColor,
+                          ),
+                        ),
+                        child: Text(
+                          "Üyelik Planları",
+                          style: TextStyle(
+                            fontFamily: "OpenDyslexic",
+                            fontSize: 12,
+                            color: ThemeConstants.creamColor,
+                          ),
+                        ),
+                      ),*/
+                    ],
                   ),
                 ),
-                SizedBox(height: 16),
-                Text(
-                  'Mevcut planınız ücretsiz plandır. Ücretli planlara yükselterek daha fazla özelliğe erişebilirsiniz.',
-                  style: TextStyle(fontFamily: "OpenDyslexic", fontSize: 14),
-                  textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 16),
+              GlassEffectContainer(
+                child: Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        'Free Plan',
+                        style: TextStyle(
+                          fontFamily: "OpenDyslexic",
+                          fontSize: 18,
+                          color: ThemeConstants.orangeColor,
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      Text(
+                        ' -Mini Oyunlara Sınırlı Erişim \n\n -Her Oyun için Ayda 20 Tur Hakkı ',
+                        style: TextStyle(
+                          fontFamily: "OpenDyslexic",
+                          fontSize: 12,
+                        ),
+                        textAlign: TextAlign.start,
+                      ),
+                      /*
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                            ThemeConstants.orangeColor,
+                          ),
+                        ),
+                        child: Text(
+                          "Üyelik Planları",
+                          style: TextStyle(
+                            fontFamily: "OpenDyslexic",
+                            fontSize: 12,
+                            color: ThemeConstants.creamColor,
+                          ),
+                        ),
+                      ),*/
+                    ],
+                  ),
                 ),
-                /*
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                      ThemeConstants.orangeColor,
-                    ),
+              ),
+              SizedBox(height: 16),
+              GlassEffectContainer(
+                child: Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        'Pro Plan',
+                        style: TextStyle(
+                          fontFamily: "OpenDyslexic",
+                          fontSize: 18,
+                          color: ThemeConstants.orangeColor,
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      Text(
+                        '\n -Ai Analizleri ve Kişiselleştirilmiş Öneriler\n \n -Profesyonellerle Eşleşip Görüşme İmkanı \n\n -Kurumsal Arayüz: Ekip Yönetimi, Çoklu Kullanıcı ve Raporlama \n\n -Öncelikli Destek ve Erken Özellikler \n\n -Oyunlara Sınırsız Erişim',
+                        style: TextStyle(
+                          fontFamily: "OpenDyslexic",
+                          fontSize: 12,
+                        ),
+                        textAlign: TextAlign.start,
+                      ),
+
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: null,
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                            const Color.fromARGB(139, 255, 138, 66),
+                          ),
+                        ),
+                        child: Text(
+                          "Çok Yakında",
+                          style: TextStyle(
+                            fontFamily: "OpenDyslexic",
+                            fontSize: 12,
+                            color: ThemeConstants.creamColor,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  child: Text(
-                    "Üyelik Planları",
-                    style: TextStyle(
-                      fontFamily: "OpenDyslexic",
-                      fontSize: 12,
-                      color: ThemeConstants.creamColor,
-                    ),
-                  ),
-                ),*/
-              ],
-            ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
